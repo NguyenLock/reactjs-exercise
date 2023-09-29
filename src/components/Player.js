2baa45629d59e891d0aa65e063275e5d3e745cec
NguyenLock <nlocnguyen9@gmail.com>
2023-09-29 14:42:54 +0700

    State Hook

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import {players} from '../shared/ListOfPlayer'
+import { useState } from 'react'
 export default function Player(){
-    
+    const [players, setPlayer] = useState([])
         return (
             <div className='container'>
                 {players.map((players)=>(
@@ -10,11 +11,20 @@ export default function Player(){
                         <img src={players.img} />
                         <h3>{players.name}</h3>
                         <p className='title'>{players.club}</p>
-                        <p><button>Detail</button></p>
+                        <button onClick={()=>{setPlayer(players)}}><a href='#popup1' id ='openPopUp'>Detail</a></button>
                     </div>
                 </div>
                 ))}
+                <div id='popup1' className='overlay'>
+                    <div className='popup'>
+                        <img src={players.img}/>
+                        <h2>{players.name}</h2>
+                        <a className='close' href='#'>&times;</a>
+                        <div className='content'>
+                            {players.info}
+                        </div>
+                    </div>
+                </div>
             </div>
         )
-    
 }
