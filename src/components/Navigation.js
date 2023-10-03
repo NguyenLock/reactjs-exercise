adc3333270946454cc15e241ae534924749a38f0
NguyenLock <nlocnguyen9@gmail.com>
2023-10-03 21:05:06 +0700

    Effect & Context

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { ThemeContext } from './ThemeContext'
+import { useContext } from 'react'
 export default function Navigation() {
+    const {theme, toggle, dark } = useContext(ThemeContext)
     return (
-        <nav>
+        <div>
+        <nav style={{backgroundColor: theme.backgroundColor, color: theme.color }}>
             <ul>
                 <li><a className='active' href='#home'>Home</a></li>
                 <li><a href='#news'>News</a></li>
                 <li><a href='#about'>Abouts</a></li>
                 <li><a href='#contact'>Contacts</a></li>
             </ul>
+            <div style={{position: 'relative'}}>
+                <a className='switch-mode' href='#' onClick={toggle}
+                style={{
+                    backgroundColor: theme.backgroundColor,
+                    color: theme.color,
+                    outline: 'none'
+                }}data-testid = "toggle-theme-btn">
+                    switch Nav to {!dark ? 'Dark' : 'Light'} Mode
+                </a>
+            </div>
         </nav>
+        </div>
     )
 }
