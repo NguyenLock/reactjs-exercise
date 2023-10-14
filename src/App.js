c42778a8fcf4c9fb1e288d2e9dd212b3eb14f5a6
NguyenLock <nlocnguyen9@gmail.com>
2023-10-14 18:30:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Detail from './components/Detail';
+import Player from './components/Player'
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import { data } from './shared/ListOfPlayer';
 function App() {
   return (
-    <div className='App'>
-      <Navigation/>
-        <Main/>
-        <Footer/>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<Player />}>
+        </Route>
+        <Route path='/detail/:id' element={<Detail />}></Route>
+        {/* <Route path='/contact' element={<Contact/>}></Route> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
